4f26c780797fb6546524d4ec23ff98fe276860ef
CoffeeWithoutSugarX <222263297+CoffeeWithoutSugarX@users.noreply.github.com>
2026-02-02 21:39:25 +0800

    Update Chat UI: Add icon buttons and adjust layout styles

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,7 +3,7 @@ import ChatHeader from "@/components/chat/chat-header";
 
 export default function ChatPage() {
     return (
-        <div>
+        <div className="flex flex-col justify-between h-screen bg-muted">
             <ChatHeader/>
             <ChatInput/>
         </div>
--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -1,10 +1,23 @@
 "use client"
 
+import {BookImage, Camera} from "lucide-react";
+
 export default function ChatInput() {
 
     return (
-        <div>
-            Chat Input
+        <div className={"pl-5 pr-5"}>
+            <div className="
+            w-full h-20
+            border-border border-t rounded-t-2xl bg-background
+            flex justify-center items-center
+            ">
+                <div className={"icon-button"}>
+                    <BookImage className={"small-icon"}/>
+                </div>
+                <div className={"icon-button"}>
+                    <Camera className={"small-icon"}/>
+                </div>
+            </div>
         </div>
     )
 }
