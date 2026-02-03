48f60b2d769904ad1261c6999cae58309a434c4e
CoffeeWithoutSugarX <222263297+CoffeeWithoutSugarX@users.noreply.github.com>
2026-02-03 13:36:47 +0800

    Render multi-line chat messages with proper line spacing.

--- src/components/chat/chat-message.tsx
+++ src/components/chat/chat-message.tsx
@@ -5,13 +5,20 @@ import ChatMessageRoleEnum from "@/components/chat/enums/ChatMessageRoleEnum";
 
 export default function ChatMessage({role, content}: ChatMessageProps) {
     const avatar = role === ChatMessageRoleEnum.USER ? <User/> : <Bot/>
+    const messageLines = content.split("\n");
     return (
-        <div className={"flex items-center gap-4 w-full pt-4 pb-4 pl-15 pr-15" + (role === ChatMessageRoleEnum.USER ? " flex-row-reverse" : "")}>
+        <div
+            className={"flex items-center gap-4 w-full pt-4 pb-4 pl-15 pr-15" + (role === ChatMessageRoleEnum.USER ? " flex-row-reverse" : "")}>
             <div className={"min-w-10 min-h-10 flex items-center justify-center rounded-full bg-foreground text-white"}>
                 {avatar}
             </div>
-            <div className={"bg-background rounded-xl p-4 border border-border shadow-lg"}>
-                {content}
+            <div className={"bg-background rounded-xl p-4 border border-border shadow-lg text-sm"}>
+                {messageLines.map((line, index) => (
+                        <div key={index} className={index != messageLines.length - 1 ? "mb-4" : ""}>
+                            {line}
+                        </div>
+                    )
+                )}
             </div>
         </div>
 
