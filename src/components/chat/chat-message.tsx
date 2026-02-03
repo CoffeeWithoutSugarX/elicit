4d75230d7ad40cdf75d1cfcde309c12d637b52b5
CoffeeWithoutSugarX <222263297+CoffeeWithoutSugarX@users.noreply.github.com>
2026-02-03 13:41:44 +0800

    Update chat message avatar text color to use `text-background` for improved consistency

--- src/components/chat/chat-message.tsx
+++ src/components/chat/chat-message.tsx
@@ -8,7 +8,7 @@ export default function ChatMessage({role, content}: ChatMessageProps) {
     const messageLines = content.split("\n");
     return (
         <div className={"flex items-center gap-4 w-full pt-4 pb-4 pl-15 pr-15" + (role === ChatMessageRoleEnum.USER ? " flex-row-reverse" : "")}>
-            <div className={"min-w-10 min-h-10 flex items-center justify-center rounded-full bg-foreground text-white"}>
+            <div className={"min-w-10 min-h-10 flex items-center justify-center rounded-full bg-foreground text-background"}>
                 {avatar}
             </div>
             <div className={"bg-background rounded-xl p-4 border border-border shadow-lg text-sm"}>
