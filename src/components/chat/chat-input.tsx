ffbbe6f7220d7df5102f0e1ac5001c0b119d8d7e
CoffeeWithoutSugarX <222263297+CoffeeWithoutSugarX@users.noreply.github.com>
2026-02-03 12:40:16 +0800

    Add reusable `Input` component and integrate with `ChatInput`

--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -1,22 +1,34 @@
 "use client"
 
-import {Camera, ImagePlus} from "lucide-react";
+import {Camera, ImagePlus, Send} from "lucide-react";
+import {Input} from "@/components/ui/input";
+import {Button} from "../ui/button";
+import {useState} from "react";
 
 export default function ChatInput() {
+
+    const [message, setMessage] = useState("")
 
     return (
         <div className={"pl-5 pr-5"}>
             <div className="
-            w-full h-20
+            max-w-2xl mx-auto h-20
             border-border border-t rounded-t-2xl bg-background
             flex justify-center items-center
             ">
                 <div className={"icon-button"}>
                     <ImagePlus className={"small-icon"}/>
                 </div>
-                <div className={"icon-button"}>
+                <div className={"icon-button mr-2"}>
                     <Camera className={"small-icon"}/>
                 </div>
+                <Input className={"max-w-[70%] text-sm mr-2 bg-muted"}
+                       placeholder={"请输入你的想法或问题..."}
+                       value={message}
+                       onChange={(e) => setMessage(e.target.value)}/>
+                <Button disabled={message.trim() === ""} className={"w-10 h-10 rounded-full flex justify-center items-center"}>
+                    <Send className={"small-icon text-background"}/>
+                </Button>
             </div>
         </div>
     )
