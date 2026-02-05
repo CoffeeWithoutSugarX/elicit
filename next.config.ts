6cf2f0e3a339aae9115107af6bf2022ac41b9f2d
CoffeeWithoutSugarX <222263297+CoffeeWithoutSugarX@users.noreply.github.com>
2026-02-05 13:54:22 +0800

    Integrate Supabase configuration for local development and add Ali-OSS functionality for secure image upload handling

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    // 强制将 ali-oss 视为服务端外部包，不进行打包处理
+    serverExternalPackages: ['ali-oss'],
 };
 
 export default nextConfig;
