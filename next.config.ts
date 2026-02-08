f2dc45467a15e0b12717b3f9d20f367569b2be15
CoffeeWithoutSugarX <lxinlucas@gmail.com>
2026-02-09 00:06:34 +0800

    Remove `allowedDevOrigins` from `next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,7 @@ const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
     // 强制将 ali-oss 视为服务端外部包，不进行打包处理
-    serverExternalPackages: ['ali-oss'],
-    allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', 'http://172.40.0.141:3000'],
+    serverExternalPackages: ['ali-oss']
 };
 
 export default nextConfig;
